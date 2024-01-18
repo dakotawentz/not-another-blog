3a64f8450e4fd965beafcc2d2dab9179314f2270
dakotawentz <dswentz@gmail.com>
2024-01-18 11:36:18 -0800

    added id, first_name, and last_name properties to user model

--- models/User.js
+++ models/User.js
@@ -4,17 +4,38 @@ const sequelize = require('../config/connection'); // Make sure to replace this 
 class User extends Model {}
 
 const User = sequelize.define('User', {
+    id: {
+      type: DataTypes.INTEGER,
+      allowNull: false,
+      primaryKey: true,
+      autoIncrement: true,
+    },
   username: {
     type: DataTypes.STRING,
     allowNull: false,
   },
-  password: {
+  first_name: {
     type: DataTypes.STRING,
     allowNull: false,
   },
-  createdAt: {
-    type: DataTypes.DATE,
-    defaultValue: DataTypes.NOW,
+  last_name: {
+    type: DataTypes.STRING,
+    allowNull: false,
+  },
+  email: {
+    type: DataTypes.STRING,
+    allowNull: false,
+    unique: true,
+    validate: {
+      isEmail: true,
+    },
+  },
+  password: {
+    type: DataTypes.STRING,
+    allowNull: false,
+    validate: {
+      len: [8],
+    },
   },
 },
 {
