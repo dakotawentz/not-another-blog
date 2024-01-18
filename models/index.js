ea8ea3a2e38d3283af373c4c1e5063dae5b4b62d
dakotawentz <dswentz@gmail.com>
2024-01-18 11:35:33 -0800

    defining how the models relate to one another within the index.js for models

--- models/index.js
+++ models/index.js
@@ -1,6 +1,34 @@
-const User = require('/User');
-const Posts = require('/Posts');
-const Comments = require('/Comments');
+const User = require('./user');
+const Posts = require('./posts');
+const Comments = require('./comments');
 
+User.hasMany(Posts, {
+    foreignKey: 'user_id',
+    onDelete: 'CASCADE',
+});
+
+Posts.belongsTo(User, {
+    foreignKey: 'user_id',
+    onDelete: 'CASCADE',
+});
+
+Posts.hasMany(Comment, {
+    foreignKey: 'user_id',
+});
+
+Comment.belongsTo(Posts, {
+    foreignKey: 'user_id',
+    onDelete: 'CASCADE',
+});
+
+User.hasMany(Comment, {
+    foreignKey: 'user_id',
+});
+
+Comment.belongsTo(User, {
+    foreignKey: 'user_id',
+    as: 'user',
+    targetKey: 'id',
+});
 
 module.exports = { User, Posts, Comments };
